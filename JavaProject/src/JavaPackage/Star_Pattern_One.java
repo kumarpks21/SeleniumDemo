b5793ec70fa749ed0bc5e85a1666614c3087177a
kumar <kumar@192.168.1.16>
2024-10-19 21:20:03 +0530

    Star Pattern

---
+++ JavaProject/src/JavaPackage/Star_Pattern_One.java
@@ -0,0 +1,12 @@
+package JavaPackage;
+
+public class Star_Pattern_One {
+public static void main(String [] args) {
+	for(int i=1;i<=5;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
---
+++ JavaProject/src/JavaPackage/Star_Pattern_Two.java
@@ -0,0 +1,19 @@
+package JavaPackage;
+
+public class Star_Pattern_Two {
+
+	public static void main(String[] args) {
+    int r=5;
+    for(int i=1; i<=r; i++) {
+    	for(int j=1; j<=r-i; j++) {
+    		System.out.print(" ");
+    	}
+    	for(int k=1; k<=i; k++) {
+    		System.out.print("*");
+    	}
+    	System.out.println();
+    }
+
+	}
+
+}
