fe9645f297f3a1526004bc8d81f597fed62f2d8f
kumar <kumar@192.168.1.16>
2024-10-19 21:49:41 +0530

    Star Pattern Six

--- JavaProject/src/JavaPackage/Star_Pattern_Six.java
+++ JavaProject/src/JavaPackage/Star_Pattern_Six.java
@@ -3,7 +3,16 @@ package JavaPackage;
 public class Star_Pattern_Six {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int r=5;
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=r-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=2*i-1; k++) {
+				System.out.print( "*");
+			}
+			System.out.println();
+		}
 
 	}
 
